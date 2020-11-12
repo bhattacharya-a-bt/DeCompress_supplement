f7c893fc3cff0aafdb9f2a08f2f6521430b9f679
Arjun Bhattacharya <bhattacharya.a.bt@gmail.com>
2020-11-12 12:24:27 -0500

    Update DeCompress_ToyExample.R

--- DeCompress_ToyExample.R
+++ DeCompress_ToyExample.R
@@ -7,7 +7,8 @@ target <- matrix(
   c(1,1,0,0,
     1,1,0,0,
     1,0,0,0,
-    0,0,1,0),
+    0,0,1,0,
+    0,1,0,1),
   ncol=4, byrow=TRUE)
 
 makeTargetSamples <- matrix(
@@ -19,6 +20,7 @@ makeTargetSamples <- matrix(
   ncol=4)
 
 targetSamples <- t(makeTargetSamples %*% t(target))
+rownames(targetSamples) <- 1:5
 #imagemat(targetSamples)
 
 colnames(targetSamples) <- 1:40
@@ -28,6 +30,7 @@ ref <- matrix(
     0,0,0,
     1,0,0,
     0,1,0,
+    0,0,1,
     1,1,0,
     1,1,0,
     1,1,0,    
@@ -49,40 +52,74 @@ refSamples <- t(makeRefSamples %*% t(ref))
 combined <- cbind(rbind(targetSamples,matrix(0,ncol=40,nrow=6)), 
                   refSamples)
 colnames(combined) <- 1:70
+rownames(combined) <- 1:11
 
 src <- rep(c("target","reference"),c(40,30))
 annoCombined <- data.frame(
   source=factor(src, levels=unique(src)),
   row.names=1:70)
 
-pheatmap(combined, annotation_col = annoCombined,
-         cluster_rows = FALSE, cluster_cols = FALSE, border=NA,
-         show_colnames = FALSE)
+geneCombined <- data.frame(
+  geneGroup=factor(rep(LETTERS[1:6], c(2,1,1,1,3,3))),
+  row.names=1:11)
+
+ann_colors = list(
+  geneGroup = RColorBrewer::brewer.pal(6, "Dark2"),
+  source = c(target="slateblue",reference="grey"),
+  cluster1 = c("1"="grey","2"="tomato2"),
+  cluster2 = c("1"="grey","2"="mediumorchid2")
+)
+names(ann_colors$geneGroup) <- LETTERS[1:6]
+
+png(file="suppfig1-1.png", width=600, height=900, res=125)
+pheatmap(combined, 
+         annotation_col = annoCombined,
+         annotation_row = geneCombined,
+         cluster_rows = FALSE, cluster_cols = FALSE, 
+         annotation_colors = ann_colors,
+         border=NA, show_colnames = FALSE, show_rownames=FALSE)
+dev.off()
 
 project <- matrix(
-  c(1,0,0,0,0,0,0,0,0,0,
-    0,1,0,0,0,0,0,0,0,0,
-    0,0,1,0,1,1,1,0,0,0,
-    0,0,0,1,1,1,1,0,0,0) + rnorm(40,0,.05),
-  ncol=10, byrow=TRUE) 
+  c(1,0,0,0,0,0,0,0,0,0,0,
+    0,1,0,0,0,0,0,0,0,0,0,
+    0,0,1,0,0,1,1,1,0,0,0,
+    0,0,0,1,0,1,1,1,0,0,0,
+    0,0,0,0,1,0,0,0,1,1,1) + rnorm(55,0,.05),
+  ncol=11, byrow=TRUE) 
 
 projectedSamples <- t(t(targetSamples) %*% project)
 colnames(projectedSamples) <- 1:40
+rownames(projectedSamples) <- 1:11
 
 #imagemat(cbind(projectedSamples, refSamples))
 
-colAnno <- data.frame(group1 = factor(rep(c(1,2,1,2), each=10)),
-                      group2 = factor(rep(1:2,each=20)),
+colAnno <- data.frame(cluster1 = factor(rep(c(1,2,1,2), each=10)),
+                      cluster2 = factor(rep(1:2,each=20)),
                       row.names=1:40)
-                                      
+
+geneTarget <- data.frame(
+  geneGroup=factor(rep(LETTERS[1:4], c(2,1,1,1))),
+  row.names=1:5)
+
+png(file="suppfig1-2.png", width=600, height=900, res=125)
 pheatmap(targetSamples, 
          cluster_rows=FALSE, 
          show_colnames=FALSE,
+         show_rownames=FALSE,
          annotation_col=colAnno,
+         annotation_row=geneTarget,
+         annotation_colors = ann_colors,
          border_color=NA)
+dev.off()
 
+png(file="suppfig1-3.png", width=600, height=900, res=125)
 pheatmap(projectedSamples, 
          cluster_rows=FALSE, 
          show_colnames=FALSE,
+         show_rownames=FALSE,
          annotation_col=colAnno,
+         annotation_row=geneCombined,
+         annotation_colors = ann_colors,
          border_color=NA)
+dev.off()
